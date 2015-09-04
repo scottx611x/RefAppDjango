8e623c513d32805b2f4602d0f028750fd7c9d7b3
scottx611x <scottx611x@gmail.com>
2015-09-04 11:53:07 -0400

    Updated App structure and changed to postgresdb

--- refineryApp/refineryApp/settings.py
+++ refineryApp/refineryApp/settings.py
@@ -77,7 +77,11 @@ WSGI_APPLICATION = 'refineryApp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.path.join(BASE_DIR, 'db.postgresql'),
+        'NAME': 'mydb',
+        'USER': 'scott',
+        'PASSWORD': 'Hockey15!',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
